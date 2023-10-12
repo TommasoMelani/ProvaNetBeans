e6206f2a21d33ed7ccf9141502ced575405157ed
Tommaso Melani <melani.tommaso2@studenti-ittfedifermi.edu.it>
2023-10-12 09:49:10 +0200

    Commit dei file comuni del progetto NetBeans

---
+++ src/github_test/GITHUB_TEST.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package github_test;
+
+/**
+ *
+ * @author acer
+ */
+public class GITHUB_TEST {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public int function1(int x) {
+        return x + 1;
+    }
+    
+    public int function2(int x) {
+        return x + 2;
+    }
+    
+}
